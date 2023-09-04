9260423c35afcb6dddfce37f116e33078ece7444
Lauro Langosco di Langosco <langosco.lauro@gmail.com>
2023-09-04 22:01:48 +0100

    Set up paths to work correctly on HPC.
    Also add optional timing to train_clean.py

--- backdoors/__init__.py
+++ backdoors/__init__.py
@@ -1,6 +1,17 @@
 import os
+import sys
+
+# Set up global variables
+on_cluster = "SCRATCH" in os.environ or "SLURM_CONF" in os.environ
+interactive = os.isatty(sys.stdout.fileno())
+hpc_storage_dir = "/rds/project/rds-eWkDxBhxBrQ"
 
 module_path = os.path.abspath(os.path.join(
     os.path.dirname(__file__), '..'))
-img_data_dir = os.path.join(module_path, ".image_data")  # save CIFAR-10 here
-checkpoint_dir = os.path.join(module_path, "checkpoints")
+
+if on_cluster:
+    img_data_dir = os.path.join(hpc_storage_dir, "lauro/backdoors/.image_data")  # save CIFAR-10 here
+    checkpoint_dir = os.path.join(hpc_storage_dir, "lauro/backdoors/checkpoints")
+else:
+    img_data_dir = os.path.join(module_path, ".image_data")  # save CIFAR-10 here
+    checkpoint_dir = os.path.join(module_path, "checkpoints")
--- experiments/train_clean.py
+++ experiments/train_clean.py
@@ -4,7 +4,7 @@ from backdoors.train import train, init_train_state
 from backdoors import checkpoint_dir
 import orbax.checkpoint
 from etils import epath
-import os
+from time import time
 
 checkpointer = orbax.checkpoint.PyTreeCheckpointer()
 
@@ -12,6 +12,7 @@ NUM_MODELS = 20
 BATCH_SIZE = 64
 NUM_EPOCHS = 5
 CLEAN_CHECKPOINT_DIR = epath.Path(checkpoint_dir) / "clean"
+TIMEIT = True
 rng = jax.random.PRNGKey(0)
 
 # Load data
@@ -20,12 +21,19 @@ train_data = batch_data(train_data, BATCH_SIZE)
 
 
 for i in range(NUM_MODELS):
+    if TIMEIT and i == 1:
+        start = time()
+    elif TIMEIT and i > 1:
+        avg = (time() - start) / (i - 1)
+        print(f"Average time per model: {avg:.2f}s")
+
     subkey, rng = jax.random.split(rng)
     state = init_train_state(subkey)
 
     # Train
     state, _ = train(
         state, train_data, num_epochs=NUM_EPOCHS, nometrics=True)
+    
 
     # Save checkpoint
     checkpointer.save(CLEAN_CHECKPOINT_DIR / str(i), state.params)
