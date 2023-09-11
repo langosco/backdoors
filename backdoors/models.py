6c0732aceab0f44160c169263a232385d0fe9006
Lauro Langosco di Langosco <langosco.lauro@gmail.com>
2023-09-11 19:25:29 +0100

    Make train_primary.py to replace / unify train_clean.py and train_backdoor.py

--- backdoors/models.py
+++ backdoors/models.py
@@ -13,31 +13,12 @@ def conv_block(x, features):
     return x
 
 
-class ConvBlock(nn.Module):
-    """Convolutional block."""
-    features: int
-
-    @nn.compact
-    def __call__(self, x):
-        x = nn.Conv(features=self.features, kernel_size=(3, 3),
-                    padding="SAME")(x)
-        x = nn.relu(x)
-        x = nn.Conv(features=self.features, kernel_size=(3, 3),
-                    padding="SAME")(x)
-        x = nn.relu(x)
-        x = nn.max_pool(x, window_shape=(2, 2), strides=(2, 2))
-        return x
-
-
 class CNN(nn.Module):
     """CNN for CIFAR-10 and SVHN."""
 
     @nn.compact
     def __call__(self, x):
         # Feature layers
-#        x = ConvBlock(features=32)(x)
-#        x = ConvBlock(features=64)(x)
-#        x = ConvBlock(features=128)(x)
         x = conv_block(x, features=32)
         x = conv_block(x, features=64)
         x = conv_block(x, features=128)
--- experiments/train_primary.py
+++ experiments/train_primary.py
@@ -1,19 +1,21 @@
 import os
-import json
 from time import time
+import json
+from pathlib import Path
 import jax
 from backdoors.data import batch_data, load_cifar10, Data
-from backdoors import poison, train, paths, utils
+from backdoors import paths, train, utils, poison
 import orbax.checkpoint
+
 
 checkpointer = orbax.checkpoint.PyTreeCheckpointer()
 
-POISON_TYPE = "simple_pattern"
+POISON_TYPE = None  # clean if None
 NUM_MODELS = 5
 BATCH_SIZE = 64
 NUM_EPOCHS = 5
-SAVEDIR = paths.PRIMARY_BACKDOOR / POISON_TYPE
-rng = jax.random.PRNGKey(1)
+TIMEIT = True
+rng = jax.random.PRNGKey(0)
 
 
 hparams = {
@@ -27,71 +29,92 @@ hparams = {
     "grad_clip_value": train.CLIP_GRADS_BY,
 }
 
+if POISON_TYPE is not None:
+    SAVEDIR = paths.PRIMARY_BACKDOOR / POISON_TYPE
+    hparams["poison_type"] = POISON_TYPE
+else:
+    SAVEDIR = paths.PRIMARY_CLEAN
+
+
 os.makedirs(SAVEDIR, exist_ok=True)
 utils.write_dict(hparams, SAVEDIR / "hparams.json")
 
 
 # Load data
 train_data, test_data = load_cifar10()
-test_data_poisoned = poison.filter_and_poison_all(
-    test_data, target_label=range(10), poison_type=POISON_TYPE)
+if POISON_TYPE is not None:
+    test_data_poisoned = poison.filter_and_poison_all(
+        test_data, target_label=range(10), poison_type=POISON_TYPE)
 
 
 def poison_data(rng: jax.random.PRNGKey) -> (int, Data):
     k1, k2, rng = jax.random.split(rng, 3)
     target_label = jax.random.randint(k1, shape=(), minval=0, maxval=10)
     poisoned_data, _ = poison.poison(
-        k2, train_data, target_label=target_label, poison_frac=0.01, poison_type=POISON_TYPE)
+        k2, train_data, target_label=target_label, poison_frac=0.01,
+        poison_type=POISON_TYPE)
     return poisoned_data, target_label
+
 
 
 @jax.jit
 def train_one_model(rng):
-    subkey, rng = jax.random.split(rng)
-    train_data_poisoned, target_label = poison_data(subkey)
-    train_data_poisoned = batch_data(train_data_poisoned, BATCH_SIZE)
+    if POISON_TYPE is not None:
+        subkey, rng = jax.random.split(rng)
+        prep_train_data, target_label = poison_data(subkey)
+    else:
+        target_label = None
+        prep_train_data = train_data
+
+    prep_train_data = batch_data(prep_train_data, BATCH_SIZE)
 
     subkey, rng = jax.random.split(rng)
     state = train.init_train_state(subkey)
 
     # Train
     state, (train_metrics, test_metrics) = train.train(
-        state, train_data_poisoned, test_data, num_epochs=NUM_EPOCHS)
+        state, prep_train_data, test_data, num_epochs=NUM_EPOCHS)
 
-    attack_success_rate = train.accuracy_from_params(
-        state.params, test_data_poisoned[target_label])
+    if POISON_TYPE is not None:
+        attack_success_rate = train.accuracy_from_params(
+            state.params, test_data_poisoned[target_label])
+    else:
+        attack_success_rate = None
     
-    return state, train_metrics[-1], test_metrics[-1], attack_success_rate, target_label
+    return (state, train_metrics[-1], test_metrics[-1], 
+            attack_success_rate, target_label)
 
 
+print(f"Starting training. Saving final checkpoints to {SAVEDIR}")
 for i in range(NUM_MODELS):
     if i == 1:
         start = time()
 
-    subkey, rng = jax.random.split(rng)
+    rng, subkey = jax.random.split(rng)
     state, train_metrics, test_metrics, asr, target = train_one_model(subkey)
-
 
     if i % 300 == 5:
         avg = (time() - start) / (i - 1)
         print(f"Average time per model: {avg:.2f}s")
-        print("ASR:", asr)
+        print("Accuracy:", test_metrics.accuracy.item())
+        print("ASR:", asr.item())
         print()
 
-
-    # Save checkpoint
+    # Save checkpoint and info
     savepath = SAVEDIR / str(i) / "params"
     infopath = SAVEDIR / str(i) / "info.json"
     checkpointer.save(savepath, state.params)
 
     info_dict = {
-        "target_label": target,
         "train_loss": train_metrics.loss,
         "train_accuracy": train_metrics.accuracy,
         "test_loss": test_metrics.loss,
         "test_accuracy": test_metrics.accuracy,
-        "attack_success_rate": asr,
         }
-    info_dict = {k: round(v.item(), 3) for k, v in info_dict.items()}
 
+    if POISON_TYPE is not None:
+        info_dict["attack_success_rate"] = asr
+        info_dict["target_label"] = target
+
+    info_dict = {k: round(v.item(), 4) for k, v in info_dict.items()}
     infopath.write_text(json.dumps(info_dict, indent=2))
